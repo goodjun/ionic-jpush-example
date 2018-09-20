dbe956f36fcae542595337d2b10c3cb7e0aa08d7
goodjun <goodjun@163.com>
2018-09-19 18:47:06 -0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,41 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { JPush } from '@jiguang-ionic/jpush';
+
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+    rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+    constructor(
+        platform: Platform,
+        statusBar: StatusBar,
+        splashScreen: SplashScreen,
+        jpush: JPush,
+    ) {
+        platform.ready().then(() => {
+
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            statusBar.styleDefault();
+            splashScreen.hide();
+
+            // jpush init
+            jpush.init().then(() => {
+                console.log('jpush init succeed');
+            }).catch(err => {
+                console.log('jpush init error');
+            })
+
+            // jpush get Registration ID
+            jpush.getRegistrationID().then(id => {
+                console.log('RegistrationID: ' + id);
+            }).catch(err => {
+                console.log('Get Registration ID Error');
+            })
+
+        });
+    }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,73 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { JPush } from '@jiguang-ionic/jpush';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(
+        public navCtrl: NavController,
+        public jpush: JPush
+    ) {
+        //
+    }
 
-  }
+    // 停止接收推送
+    stopPush() {
+        this.jpush.stopPush().then(() => {
+            console.log('stopPush succeed');
+        }).catch(err => {
+            console.log('stopPush error');
+        })
+    }
+
+    // 继续接收推送
+    resumePush() {
+        this.jpush.resumePush().then(() => {
+            console.log('resumePush succeed');
+        }).catch(err => {
+            console.log('resumePush error');
+        })
+    }
+
+    // 设置badge
+    setBadge() {
+        this.jpush.setBadge(99).then(() => {
+            console.log('setBadge succeed');
+        }).catch(err => {
+            console.log('setBadge error');
+        })
+    }
+
+    // 重置badge
+    resetBadge() {
+        this.jpush.resetBadge().then(() => {
+            console.log('resetBadge succeed');
+        }).catch(err => {
+            console.log('resetBadge error');
+        })
+    }
+
+    // 清空所有推送
+    clearAllNotification() {
+        this.jpush.clearAllNotification().then(() => {
+            console.log('clearAllNotification succeed');
+        }).catch(err => {
+            console.log('clearAllNotification error');
+        })
+    }
+
+    // 推送一条本地推送
+    addLocalNotification() {
+        this.jpush.addLocalNotificationForIOS(0, 'test push', 1, 'uuid').then(() => {
+            console.log('addLocalNotification succeed');
+        }).catch(err => {
+            console.log('addLocalNotification error');
+        })
+    }
 
 }
